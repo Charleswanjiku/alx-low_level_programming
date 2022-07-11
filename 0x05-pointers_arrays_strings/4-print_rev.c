4d811dc45ad48da5bf90bede39219d6b0c9ddb68
Charleswanjiku <kangethewanjiku@gmail.com>
2022-07-11 13:00:10 +0000

    # C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,13 @@
 
 void print_rev(char *s)
 {
-	int i;
+int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-		;
+for (i = 0; s[i] != '\0'; i++)
+;
 
-	for (i--; i >= 0; i--)
-		_putchar(s[i]);
-	
-	_putchar('\n');
+for (i--; i >= 0; i--)
+_putchar(s[i]);
+
+_putchar('\n');
 }
