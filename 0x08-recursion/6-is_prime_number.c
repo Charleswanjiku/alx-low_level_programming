c64fd1b2f4389705363f3bc48f70557db3f9e4d4
Charleswanjiku <kangethewanjiku@gmail.com>
2022-07-20 11:55:06 +0000

    fix 06

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,51 +1,38 @@
 #include "main.h"
+#include <stdio.h>
+
+int check_prime(int n, int i);
 
 /**
- * is_prime_number - determine if a number is a prime number
- * @n: int number
- * Return: 1 if prime, 0 otherwise
+ * is_prime_number - Returns if a number is prime
+ * @n: the number to be checked
+ *
+ * Return: integer value
  */
 
 int is_prime_number(int n)
 {
-	if (n < 2)
-		return (0);
-	if (n < 4)
-		return (1);
-	return (hai(n, 2));
+	return (check_prime(n, 1));
 }
 
 /**
- * _sqrt - return square root of number
- * @x: number
- * @i: number incrementer acting as divisor
- * Return: square root of `x`
+ * check_prime - Check if number is prime
+ * @n: the number to be checked
+ * @i: the iteration times
+ *
+ * Return: 1 for prime or 0 composite
  */
 
-int _sqrt(int x, int i)
+int check_prime(int n, int i)
 {
-	int square;
-
-	square = i * i;
-	if (square >= x)
-		return (i);
-	else
-		return (_sqrt(x, i + 1));
-}
-
-/**
- * hai - helper function, recursive steps taken
- * @n: number given to original function is_prime_number
- * @d: incrementer divisor
- * Return: 0 if not prime, 1 if prime
- */
-
-int hai(int n, int d)
-{
-	if (n % d == 0)
+	if (n <= 1)
 		return (0);
-	if (_sqrt(n, 1) < d)
+
+	if (n % i == 0 && i > 1)
+		return (0);
+
+	if ((n / i) < i)
 		return (1);
-	else
-		return (hai(n, d + 1));
+
+	return (check_prime(n, i + 1));
 }
