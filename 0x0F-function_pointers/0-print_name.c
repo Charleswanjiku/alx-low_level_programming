d75e80ecdfca23f5cde5050bc38b8916e29acedf
Charleswanjiku <kangethewanjiku@gmail.com>
2022-08-03 12:18:58 +0000

    FIX

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,11 @@
  * print_name - Print a name from a function pointer
  * @name: char string
  * @f: functions pointer that takes a string argument
+ * Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != 0)
+		f(name);
 }
